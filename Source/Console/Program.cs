312678c1e409c67fcd83eb75037e80a97645e17b
Sebastian Studniczek <sebastian.studniczek@outlook.com>
2023-09-16 19:28:24 +0200

    Init

---
+++ Source/Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StravaActivityExtractor.Core;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        services.Configure<StravaOptions>(
+            ctx.Configuration.GetSection(StravaOptions.Strava));
+
+        services.AddCore();
+    })
+    .Build();
